9aa45efdd0773c6b9cbf4992431d3a5199e1aecd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-17 20:34:17 +0300

    .

--- resources/highlight-code.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('code[class^="language-"]').forEach(function(element) {
-    var c = element.getAttribute('class');
-    element.setAttribute('class', c.replace(/language-/g, ''));
-
-    hljs.highlightElement(element);
-});
